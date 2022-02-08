872be8610e574beacfb638939cd3ea45f6518b73
Blake Ward <26913683+Nevvulo@users.noreply.github.com>
2022-02-08 21:33:38 +1100

    Update workflow + scripts

--- scripts/upsert-post-in-blogmap.sh
+++ scripts/upsert-post-in-blogmap.sh
@@ -20,7 +20,7 @@ jq -M '
   | reverse' post-properties.json > updated-properties.json
 
 echo $(cat updated-properties.json)
-[ -n "$SLUG" ] && echo $SLUG || echo 'Slug not found' && exit 1
+[ -n "$SLUG" ] && echo $SLUG || (echo 'Slug not found' && exit 1)
 BLOGMAP=$(jq --slurpfile post updated-properties.json 'del(.[] | select(.slug == "'"$SLUG"'")) | . += $post[]' blogmap.json -c)
 [ -n "$BLOGMAP" ] && echo 'Blogmap set' || exit 1
 echo $BLOGMAP > blogmap.json
